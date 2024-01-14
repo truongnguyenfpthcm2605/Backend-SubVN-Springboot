95ff13e025d8754010890495c49ca761f9e23af6
nhor099 <18110137@student.hcmute.edu.vn>
2024-01-14 17:41:11 +0700

    no message

---
+++ src/main/java/com/subvn/backendsubvnspringboot/BackendSubVnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.subvn.backendsubvnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSubVnSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSubVnSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subvn/backendsubvnspringboot/BackendSubVnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subvn.backendsubvnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSubVnSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
